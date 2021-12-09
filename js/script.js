c025057533445072a7b2c8b54651232a47d0082b
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-09 15:37:18 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const app = new Vue({
         for(let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                console.log(response.data.response);
                 this.emails.push(response.data.response);
             })
             .catch(function (error) {
