8b0743dc3ab3a2a7864fc2a851694a68bafb797f
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-09 15:25:18 +0100

    get data from api

--- js/script.js
+++ js/script.js
@@ -5,5 +5,14 @@ const app = new Vue({
     },
     methods : {
 
+    },
+    created : function() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 })
